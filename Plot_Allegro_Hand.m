48238ee1d7ce48b09061ab8e9cbe43306d061d94
chrystalight <140845359+chrystalight@users.noreply.github.com>
2025-11-18 23:31:46 -0500

    made sure save and import worked right

--- Plot_Allegro_Hand.m
+++ Plot_Allegro_Hand.m
@@ -1,8 +1,4 @@
-%    SGexample_allegro_grasp - Basic example of a grasp with the Allegro Hand
-%
-%    Usage: SGexample_allegro_grasp
-%
-%    Authors:
+
 %    M. Pozzi, M. Malvezzi, D. Prattichizzo
 %    
 %    References:
@@ -15,34 +11,10 @@
 %    Ajoudani. Grasp Stiffness Control in Robotic Hands through Coordinated  
 %    Optimization of Pose and Joint Stiffness. IEEE Robotics and Automation 
 %    Letters, 3(4):3952-3959, October 2018.
-%
-%  Redistribution and use with or without
-%  modification, are permitted provided that the following conditions are met:
-%      * Redistributions of source code must retain he above copyright
-%        notice, this list of conditions and the following disclaimer.
-%      * Redistributions in binary form must reproduce the above copyright
-%        notice, this list of conditions and the following disclaimer in the
-%        documentation and/or other materials provided with the distribution.
-%      * Neither the name of the <organization> nor the
-%        names of its contributors may be used to endorse or promote products
-%        derived from this software without specific prior written permission.
-% 
-%  THIS SOFTWARE IS PROVIDED BY M. Malvezzi, G. Gioioso, G. Salvietti, D.
-%  Prattichizzo, ``AS IS'' AND ANY
-%  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-%  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-%  DISCLAIMED. IN NO EVENT SHALL M. Malvezzi, G. Gioioso, G. Salvietti, D.
-%  Prattichizzo BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
-%  EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-%  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-%  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-%  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-%  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-%  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-close all
-clear all
-clc
+% close all
+% clear all
+% clc
 
 %% HAND DEFINITION 
 T_hand_ini=[SGrotz(pi/2)*SGrotx(pi/2)*SGroty(pi/2) [0 0 0]';
@@ -120,6 +92,3 @@ SGplotContactPoints(hand,10,'o')
 % Matrices for grasp analysis
 Hand_Jacobian = hand.J;
 Grasp_Matrix = obj.G;
-
-NGT=ker(Grasp_Matrix');
-NJT=ker(Hand_Jacobian');
